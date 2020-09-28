138fffb6606eafc96208a75a7b762a823ca1c270
motclink <39547783+motclink@users.noreply.github.com>
2020-09-28 15:42:06 +0800

    Add files via upload

---
+++ decoder/src/main/java/openlr/decoder/properties/OpenLRDecoderProperties.java
@@ -0,0 +1,345 @@
+/**
+ * Licensed to the TomTom International B.V. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  TomTom International B.V.
+ * licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * <p>
+ * Copyright (C) 2009-2019 TomTom International B.V.
+ * <p>
+ * TomTom (Legal Department)
+ * Email: legal@tomtom.com
+ * <p>
+ * TomTom (Technical contact)
+ * Email: openlr@tomtom.com
+ * <p>
+ * Address: TomTom International B.V., Oosterdoksstraat 114, 1011DK Amsterdam,
+ * the Netherlands
+ */
+package openlr.decoder.properties;
+
+import openlr.OpenLRProcessingException;
+import openlr.decoder.rating.OpenLRRating.RatingCategory;
+import openlr.map.FunctionalRoadClass;
+import openlr.properties.OpenLRPropertyAccess;
+import org.apache.commons.configuration.Configuration;
+
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The Class OpenLRDecoderProperties.
+ */
+public class OpenLRDecoderProperties {
+
+    /** The bearing distance. */
+    private final int bearingDistance;
+
+    /** The max node distance. */
+    private final int maxNodeDistance;
+
+    /** The node factor. */
+    private final int nodeFactor;
+
+    /** The line factor. */
+    private final int lineFactor;
+
+    /** The frc variance. */
+    private final Map<FunctionalRoadClass, Integer> frcVariance;
+
+    /** The minimum accepted rating. */
+    private final int minimumAcceptedRating;
+
+    /** The max number retries. */
+    private final int maxNumberRetries;
+
+    /** The connected route increase. */
+    private final float connectedRouteIncrease;
+
+    /** The dnp variance. */
+    private final int dnpVariance;
+
+    /** The max bearing diff. */
+    private final int maxBearingDiff;
+
+    /** The maximum score bearing rating can contribute to the overall rating. */
+    private final int maxBearingScore;
+
+    /** The frc rating. */
+    private final EnumMap<RatingCategory, Integer> frcRating;
+
+    /** The frc intervals. */
+    private final EnumMap<RatingCategory, Integer> frcIntervals;
+
+    /** The fow rating. */
+    private final EnumMap<RatingCategory, Integer> fowRating;
+
+    /** The calc affected lines. */
+    private final boolean calcAffectedLines;
+
+    /** The non junction node factor. */
+    private final float nonJunctionNodeFactor;
+
+    /** The lines directly factor. */
+    private final float linesDirectlyFactor;
+
+    /** The comp time4 cache. */
+    private final int compTime4Cache;
+
+    /**
+     * Instantiates a new open lr decoder properties.
+     *
+     * @param config
+     *            the config
+     * @throws OpenLRProcessingException
+     *             the open lr processing exception
+     */
+    public OpenLRDecoderProperties(final Configuration config)
+            throws OpenLRProcessingException {
+        bearingDistance = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.BEAR_DIST);
+        maxNodeDistance = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.MAX_NODE_DIST);
+        nodeFactor = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.NODE_FACTOR);
+        lineFactor = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.LINE_FACTOR);
+        nonJunctionNodeFactor = OpenLRPropertyAccess.getFloatPropertyValue(config,
+                OpenLRDecoderProperty.NON_JUNCTION_NODE_FACTOR);
+
+        frcVariance = new HashMap<>();
+
+        for (FunctionalRoadClass frc : FunctionalRoadClass.values()) {
+            int variance = OpenLRPropertyAccess.getIntegerPropertyValueFromMap(
+                    config,
+                    OpenLRDecoderProperty.FRC_VARIANCE,
+                    frc.name());
+            frcVariance.put(frc, variance);
+        }
+
+        minimumAcceptedRating = OpenLRPropertyAccess.getIntegerPropertyValue(
+                config, OpenLRDecoderProperty.MIN_ACC_RATING);
+        maxNumberRetries = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.MAX_NR_RETRIES);
+        connectedRouteIncrease = OpenLRPropertyAccess.getFloatPropertyValue(
+                config, OpenLRDecoderProperty.CONNECT_ROUTE_INC);
+        dnpVariance = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.DNP_VARIANCE);
+        maxBearingDiff = OpenLRPropertyAccess.getIntegerPropertyValueFromMap(config, OpenLRDecoderProperty.BEAR_RATING, "MaxBearingDiff");
+        maxBearingScore = OpenLRPropertyAccess.getIntegerPropertyValueFromMap(config, OpenLRDecoderProperty.BEAR_RATING, "MaxScore");
+
+        frcRating = new EnumMap<RatingCategory, Integer>(RatingCategory.class);
+        fowRating = new EnumMap<RatingCategory, Integer>(RatingCategory.class);
+        frcIntervals = new EnumMap<RatingCategory, Integer>(
+                RatingCategory.class);
+        for (RatingCategory cat : RatingCategory.values()) {
+            fowRating.put(cat, OpenLRPropertyAccess
+                    .getIntegerPropertyValueFromMap(config,
+                            OpenLRDecoderProperty.FOW_RATING,
+                            cat.getIdentifier()));
+            frcRating.put(cat, OpenLRPropertyAccess
+                    .getIntegerPropertyValueFromMap(config,
+                            OpenLRDecoderProperty.FRC_RATING,
+                            cat.getIdentifier()));
+            if (cat != RatingCategory.POOR) {
+                frcIntervals.put(cat, OpenLRPropertyAccess
+                        .getIntegerPropertyValueFromMap(config,
+                                OpenLRDecoderProperty.FRC_INTERVALS,
+                                cat.getIdentifier()));
+            }
+        }
+
+        calcAffectedLines = OpenLRPropertyAccess.getBooleanPropertyValue(
+                config, OpenLRDecoderProperty.CALC_AFFECTED_LINES);
+        linesDirectlyFactor = OpenLRPropertyAccess.getFloatPropertyValue(
+                config, OpenLRDecoderProperty.LINES_DIRECTLY_FACTOR);
+        compTime4Cache = OpenLRPropertyAccess.getIntegerPropertyValue(config,
+                OpenLRDecoderProperty.COMP_TIME_4_CACHE);
+    }
+
+    /**
+     * Gets the bearing distance.
+     *
+     * @return the bearingDistance
+     */
+    public final int getBearingDistance() {
+        return bearingDistance;
+    }
+
+    /**
+     * Gets the max node distance.
+     *
+     * @return the maxNodeDistance
+     */
+    public final int getMaxNodeDistance() {
+        return maxNodeDistance;
+    }
+
+    /**
+     * Gets the node factor.
+     *
+     * @return the nodeFactor
+     */
+    public final int getNodeFactor() {
+        return nodeFactor;
+    }
+
+    /**
+     * Gets the line factor.
+     *
+     * @return the lineFactor
+     */
+    public final int getLineFactor() {
+        return lineFactor;
+    }
+
+    /**
+     * Get the non-junction node factor. This is a value between 0 and 1 and is the scaling value to apply
+     * during decoding to the score of nodes that are not junctions.
+     *
+     * By default the non-junction node factor is 1, meaning that non-junction nodes are not penalised.
+     * To apply a penalty to non-junction nodes, reduce the non-junction node factor to a value under 1.
+     *
+     * @return the non-junction node factor
+     */
+    public final float getNonJunctionNodeFactor() {
+        return nonJunctionNodeFactor;
+    }
+
+    /**
+     * Gets the frc variance.
+     *
+     * @return the frcVariance
+     */
+    public final int getFrcVariance(FunctionalRoadClass frc) {
+        return frcVariance.get(frc);
+    }
+
+    /**
+     * Gets the minimum accepted rating.
+     *
+     * @return the minimumAcceptedRating
+     */
+    public final int getMinimumAcceptedRating() {
+        return minimumAcceptedRating;
+    }
+
+    /**
+     * Gets the max number retries.
+     *
+     * @return the maxNumberRetries
+     */
+    public final int getMaxNumberRetries() {
+        return maxNumberRetries;
+    }
+
+    /**
+     * Gets the connected route increase.
+     *
+     * @return the connectedRouteIncrease
+     */
+    public final float getConnectedRouteIncrease() {
+        return connectedRouteIncrease;
+    }
+
+    /**
+     * Gets the dnp variance.
+     *
+     * @return the dnpVariance
+     */
+    public final int getDnpVariance() {
+        return dnpVariance;
+    }
+
+    /**
+     * Gets the max bearing diff.
+     *
+     * @return the maxBearingDiff
+     */
+    public final int getMaxBearingDiff() {
+        return maxBearingDiff;
+    }
+
+    /**
+     * Gets the max bearing score.
+     *
+     * @return the maxBearingDiff
+     */
+    public final int getMaxBearingScore() {
+        return maxBearingScore;
+    }
+
+    /**
+     * Gets the frc rating.
+     *
+     * @param cat
+     *            the cat
+     * @return the frcRating
+     */
+    public final int getFrcRating(final RatingCategory cat) {
+        return frcRating.get(cat);
+    }
+
+    /**
+     * Gets the frc intervals.
+     *
+     * @param interval
+     *            the interval
+     * @return the frcIntervals
+     */
+    public final int getFrcIntervals(final RatingCategory interval) {
+        return frcIntervals.get(interval);
+    }
+
+    /**
+     * Gets the fow rating.
+     *
+     * @param cat
+     *            the cat
+     * @return the fowRating
+     */
+    public final int getFowRating(final RatingCategory cat) {
+        return fowRating.get(cat);
+    }
+
+
+    /**
+     * Checks if is calc affected lines.
+     *
+     * @return the calcAffectedLines
+     */
+    public final boolean isCalcAffectedLines() {
+        return calcAffectedLines;
+    }
+
+    /**
+     * Gets the lines directly factor.
+     *
+     * @return the linesDirectlyFactor
+     */
+    public final float getLinesDirectlyFactor() {
+        return linesDirectlyFactor;
+    }
+
+    /**
+     * Gets the comp time4 cache.
+     *
+     * @return the compTime4Cache
+     */
+    public final int getCompTime4Cache() {
+        return compTime4Cache;
+    }
+
+}
---
+++ decoder/src/main/java/openlr/decoder/rating/OpenLRRatingImpl.java
@@ -0,0 +1,304 @@
+/**
+ * Licensed to the TomTom International B.V. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  TomTom International B.V.
+ * licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * <p>
+ * Copyright (C) 2009-2019 TomTom International B.V.
+ * <p>
+ * TomTom (Legal Department)
+ * Email: legal@tomtom.com
+ * <p>
+ * TomTom (Technical contact)
+ * Email: openlr@tomtom.com
+ * <p>
+ * Address: TomTom International B.V., Oosterdoksstraat 114, 1011DK Amsterdam,
+ * the Netherlands
+ * <p>
+ */
+package openlr.decoder.rating;
+
+import openlr.LocationReferencePoint;
+import openlr.OpenLRProcessingException;
+import openlr.decoder.properties.OpenLRDecoderProperties;
+import openlr.map.FormOfWay;
+import openlr.map.FunctionalRoadClass;
+import openlr.map.Line;
+import openlr.map.Node;
+import openlr.map.utils.GeometryUtils;
+import openlr.map.utils.GeometryUtils.BearingDirection;
+import org.apache.log4j.Logger;
+
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * The Class OpenLRRatingImpl implements the rating function for OpenLR.
+ * <p>
+ * This implementation makes use of the following attributes:
+ * <ul>
+ * <li>distance between the LRP position and the corresponding node of the line
+ * <li>differences in the functional road class
+ * <li>differences in the form of way
+ * <li>differences in the bearing
+ * </ul>
+ * The formula used for the rating value looks like:<br>
+ * <br>
+ * [node_factor] * node_value + [line_factor] * (bearing_value + frc_value +
+ * fow_value) <br>
+ * <br>
+ * The factors are defined in the OpenLR properties. The values are based on a
+ * classification how good the actual line value matches the LRP attributes. The
+ * possible categories are defined in {@link OpenLRRating.RatingCategory} and
+ * the rating values are also defined in the OpenLR properties. <br>
+ * <p>
+ * OpenLR is a trade mark of TomTom International B.V.
+ * <p>
+ * email: software@openlr.org
+ *
+ * @author TomTom International B.V.
+ */
+public class OpenLRRatingImpl implements OpenLRRating {
+
+    /** Number of degrees for a half circle */
+    private static final int HALF_CIRCLE = 180;
+
+    /** Number of degrees for a full circle */
+    private static final int FULL_CIRCLE = 360;
+
+    /** logger */
+    private static final Logger LOG = Logger.getLogger(OpenLRRatingImpl.class);
+
+    /** The Constant fowRatingTable. */
+    private static final FormOfWayRatingTable FOW_RATING_TABLE = new FormOfWayRatingTable();
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getRating(final OpenLRDecoderProperties properties,
+                               final int distance, final LocationReferencePoint p,
+                               final Line line, final int projectionAlongLine)
+            throws OpenLRProcessingException {
+        BearingDirection dir = null;
+        if (p.isLastLRP()) {
+            dir = BearingDirection.AGAINST_DIRECTION;
+        } else {
+            dir = BearingDirection.IN_DIRECTION;
+        }
+
+        int nodeRating = calculateDistanceRating(properties, distance);
+
+        if (shouldApplyNonJunctionNodeFactor(line, dir, projectionAlongLine)) {
+            nodeRating = (int) (properties.getNonJunctionNodeFactor() * nodeRating);
+        }
+
+        int bearingRating = calculateBearingRating(properties, p.getBearing(),
+                dir, line, projectionAlongLine);
+        if (bearingRating < 0) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("bearing of a candidate line is out of range ["
+                        + line.getID() + "]");
+            }
+            return -1;
+        }
+        int frcRating = calculateFRCRating(properties, p.getFRC(), line);
+        int fowRating = calculateFOWRating(properties, p.getFOW(), line);
+        int lineRating = bearingRating + frcRating + fowRating;
+        int rating = properties.getNodeFactor() * nodeRating
+                + properties.getLineFactor() * lineRating;
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("total rating [" + line.getID() + "]: " + rating
+                    + "  (node: " + nodeRating + ", line: " + lineRating
+                    + ", bearing: " + bearingRating + ", frc: " + frcRating
+                    + ", fow: " + fowRating + ")");
+        }
+        return rating;
+    }
+
+    /**
+     * Determine whether to apply the non-junction node factor to the node score
+     *
+     * @param line the line under consideration
+     * @param dir the direction along the line
+     * @param projectionAlongLine the projected distance along the line
+     * @return true if the non-junction node factor is to be applied
+     */
+    private boolean shouldApplyNonJunctionNodeFactor(Line line, BearingDirection dir, int projectionAlongLine) {
+        // Only apply the non-junction node factor when the LRP matches a node and not a line directly
+        if (projectionAlongLine > 0 && projectionAlongLine < line.getLineLength()) {
+            return false;
+        }
+
+        // Find the node to check
+        Node node = dir == BearingDirection.IN_DIRECTION ? line.getStartNode() : line.getEndNode();
+
+        // Check if the node is not a junction
+        return !isJunction(node);
+    }
+
+    /**
+     * Check if a node is a junction. Junction nodes are nodes which are directly connected to more than 2 nodes.
+     *
+     * @param node the node to check
+     * @return true if the node is a junction
+     */
+    private boolean isJunction(Node node) {
+        return getConnectedNodeCount(node) > 2;
+    }
+
+    /**
+     * Get the number of nodes that are directly connected to a node via its connected lines
+     *
+     * @param node the node
+     * @return the number of connected nodes
+     */
+    private int getConnectedNodeCount(Node node) {
+        return (int) StreamSupport.stream(Spliterators.spliteratorUnknownSize(node.getConnectedLines(), Spliterator.ORDERED), false)
+                .flatMap(l -> Stream.of(l.getStartNode(), l.getEndNode()))
+                .filter(n -> !node.equals(n))
+                .distinct()
+                .count();
+    }
+
+    /**
+     * Calculates the node value based on the distance between the LRP position
+     * and the corresponding node. The formula looks like: <br>
+     * <br>
+     * node_value = [max_node_distance] - distance <br>
+     * <br>
+     * The max_node_distance is configurable in the OpenLR properties.
+     *
+     * @param properties
+     *            the OpenLR properties
+     * @param distance
+     *            the distance of the node to the LRP position
+     * @return the node value
+     * @throws OpenLRProcessingException
+     *             the open lr processing exception
+     */
+    private int calculateDistanceRating(
+            final OpenLRDecoderProperties properties, final double distance)
+            throws OpenLRProcessingException {
+        int diff = properties.getMaxNodeDistance() - (int) Math.round(distance);
+        return Math.max(0, diff);
+    }
+
+    /**
+     * Calculates the frc value based on the frc values of the line and the lrp
+     * attribute. <br>
+     * <br>
+     * The method calculates the difference of the frc IDs and looks up the
+     * rating category for this difference value. The defined value based on
+     * this category is used for the rating value. The rating categories and its
+     * rating values are defined in the OpenLR properties.
+     *
+     * @param properties
+     *            the OpenLR properties
+     * @param frc
+     *            the functional road class of the LRP
+     * @param line
+     *            the line to be rated
+     * @return the frc value
+     * @throws OpenLRProcessingException
+     *             the open lr processing exception
+     */
+    private int calculateFRCRating(final OpenLRDecoderProperties properties,
+                                   final FunctionalRoadClass frc, final Line line)
+            throws OpenLRProcessingException {
+        int lineFRC = line.getFRC().getID();
+        int diff = Math.abs(frc.getID() - lineFRC);
+
+        RatingCategory bestCat;
+        if (diff <= properties.getFrcIntervals(RatingCategory.EXCELLENT)) {
+            bestCat = RatingCategory.EXCELLENT;
+        } else if (diff <= properties.getFrcIntervals(RatingCategory.GOOD)) {
+            bestCat = RatingCategory.GOOD;
+        } else if (diff <= properties.getFrcIntervals(RatingCategory.AVERAGE)) {
+            bestCat = RatingCategory.AVERAGE;
+        } else {
+            bestCat = RatingCategory.POOR;
+        }
+        return properties.getFrcRating(bestCat);
+    }
+
+    /**
+     * Calculates the fow value based on the line and lrp attributes. The
+     * formula uses the rating values defined in the OpenLR properties. The
+     * matching between the line and lrp fow attribute is EXCELLENT if both IDs
+     * are equal. Otherwise if one of the attributes is undefined then the match
+     * is AVERAGE and if no former rule matches then we have a POOR match of the
+     * attributes.
+     *
+     * @param properties
+     *            the OpenLR properties
+     * @param fow
+     *            the fow attribute of the LRP
+     * @param line
+     *            the line to be rated
+     * @return the fow value
+     * @throws OpenLRProcessingException
+     *             the open lr processing exception
+     */
+    private int calculateFOWRating(final OpenLRDecoderProperties properties,
+                                   final FormOfWay fow, final Line line)
+            throws OpenLRProcessingException {
+        RatingCategory category = FOW_RATING_TABLE.getRating(fow, line.getFOW());
+        return properties.getFowRating(category);
+    }
+
+    /**
+     * Calculates the bearing value based on the bearing values of the line and
+     * the lrp attribute. <br>
+     * <br>
+     * The method calculates the difference of the bearing values and looks up
+     * the rating category for this difference value. The defined value based on
+     * this category is used for the rating value. The rating categories and its
+     * rating values are defined in the OpenLR properties.
+     *
+     * Candidate lines with a bearing difference greater than the defined value
+     * will be rejected.
+     *
+     * @param properties
+     *            the OpenLR properties
+     * @param bearing
+     *            the bearing value of the LRP
+     * @param dir
+     *            the bearing direction (in direction or against direction)
+     * @param line
+     *            the line to be rated
+     * @param projectionAlongLine
+     *            the distance between the projection point and the start of the
+     *            line
+     * @return the bearing value
+     * @throws OpenLRProcessingException
+     *             the open lr processing exception
+     */
+    private int calculateBearingRating(
+            final OpenLRDecoderProperties properties, final double bearing,
+            final BearingDirection dir, final Line line,
+            final int projectionAlongLine) throws OpenLRProcessingException {
+        double lineBearing = GeometryUtils.calculateLineBearing(line, dir,
+                properties.getBearingDistance(), projectionAlongLine);
+
+        double difference = GeometryUtils.bearingDifference(lineBearing, bearing);
+
+        if (difference > properties.getMaxBearingDiff()) {
+            return 0;
+        }
+        return (int) ((1.0 - difference / properties.getMaxBearingDiff()) * properties.getMaxBearingScore());
+    }
+
+}
